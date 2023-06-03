6b0e7f89fa10c14fd2b54f2522816feabcc097b9
vijaykumararik <vijaykumararikatti@gmail.com>
2023-06-03 12:13:20 +0530

    done anotation

--- src/main/java/com/Adharcard.java
+++ src/main/java/com/Adharcard.java
@@ -1,5 +1,26 @@
 package com;
 
 public class Adharcard {
+private int anumber;
+private String state;
+private String address;
+public int getAnumber() {
+	return anumber;
+}
+public void setAnumber(int anumber) {
+	this.anumber = anumber;
+}
+public String getState() {
+	return state;
+}
+public void setState(String state) {
+	this.state = state;
+}
+public String getAddress() {
+	return address;
+}
+public void setAddress(String address) {
+	this.address = address;
+}
 
 }
--- src/main/java/com/Bike.java
+++ src/main/java/com/Bike.java
@@ -1,5 +1,33 @@
 package com;
 
 public class Bike {
-
+ private int id;
+ private String name;
+ private int price;
+ private String modal;
+public int getId() {
+	return id;
+}
+public void setId(int id) {
+	this.id = id;
+}
+public String getName() {
+	return name;
+}
+public void setName(String name) {
+	this.name = name;
+}
+public int getPrice() {
+	return price;
+}
+public void setPrice(int price) {
+	this.price = price;
+}
+public String getModal() {
+	return modal;
+}
+public void setModal(String modal) {
+	this.modal = modal;
+}
+ 
 }
